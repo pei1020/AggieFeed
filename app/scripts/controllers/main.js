8b2d46ddf8bd0373a78384991f44862cb791833e
pei1020 <peiliu@ucdavis.edu>
2017-05-15 16:25:19 -0700

    ajax part

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,14 @@
 angular.module('MyApp')
   .controller('MainCtrl', function ($scope, $http, ServiceCtrl)
 {
-
-    ServiceCtrl.getWeather('#cityname'.value);
-});
+     $scope.go = function() {
+    //  $scope.msg = document.getElementById('cityname').value;
+      ServiceCtrl.current_weather(function(data){
+            $scope.city = data.name;
+            $scope.temperature = data.main.temp;
+            $scope.current_weather = data.weather[0].main;
+            $scope.description = data.weather[0].description;
+            // console.log(data.weather[0].main);
+        });
+      };
+   });
--- app/scripts/service.js
+++ app/scripts/service.js
@@ -10,18 +10,19 @@
 angular.module('MyApp')
   .service('ServiceCtrl', function ()
 {
-  return{
-    getWeather: function(cityname){
-    var cityname = $('#cityname').value;
+   this.current_weather = getWeather;
+    function getWeather(callback){
+    var cityname = document.getElementById('cityname').value;
         $.ajax({
           type: 'GET',
           url: "http://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=a57c72744b6d58125f048dbf8ef60523",
-          success:function(city) {
-            console.log(city.main.temp);
-            return city.main.temp;
+          success:function(data, display) {
+            // console.log(city.main.temp);
+            // console.log(city.weather[0].main);
+            //  console.log(data.name);
+            // $scope.city = city.name;
+            callback(data);
             }
-    })
-
-      }
-};
+          })
+    };
 });
